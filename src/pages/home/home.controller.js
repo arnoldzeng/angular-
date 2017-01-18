30f8892db489d61223dfd1583d025ebe27f4ab33
firstgit <505257618@qq.com>
2017-01-18 16:53:00 +0800

    修改样式，增加注释

--- src/pages/home/home.controller.js
+++ src/pages/home/home.controller.js
@@ -11,7 +11,7 @@ export default class HomeController {
     this.newItems = []
     // this.items = this.getdata()
   }
-
+  // 初始化
   $onInit() {
     // this.act().
     // this.Test()
@@ -29,16 +29,15 @@ export default class HomeController {
     //  this.setdata('key', 'val')
     // })
   }
-
+  // 随机提取名字
   randomName() {
     this.name = this.user.getName()
   }
-
+  // 删除按钮
   del(index, e) {
     e.stopPropagation()
     // let item = this.items[index]
     // this.localStorage.claerItem(tem)
-    console.log(this.items[0])
     this.items.splice(index, 1)
     this.newItems = this.items
     console.log(this.newItems)
@@ -49,19 +48,23 @@ export default class HomeController {
     this.t = this.localStorage.test()
     return this.t
   }
+  stopBubble(e) {
+    e.stopPropagation()
+  }
 
+  //  提取数据
   getList() {
     this.items = this.localStorage.fetch('todo')
   }
-
+  // 保存一个新的数据
   newSetLsit() {
     this.localStorage.save('todo', this.newItems)
   }
-
+  // 保存数据
   setList() {
     this.localStorage.save('todo', this.items)
   }
-
+  // 输入回车后，保存数据
   enter(e) {
     // var keycode = window.event ? e.keyCode : e.which
     if (e.keyCode === 13) {
@@ -79,7 +82,7 @@ export default class HomeController {
     console.log(this.localStorage.claerItem(this.items))
     console.log(this.items.length)
   }
-
+  // 清空数据
   Clear() {
     // this.items = this.localStorage.clear(this.items)
     // this.localStorage.claer(this.items)
@@ -88,9 +91,10 @@ export default class HomeController {
     this.items = []
     // this.items = null
   }
-
-  change(tem) {
+  // 点击改变 样式
+  change(tem, e) {
     // tem.isfinsh
+    e.stopPropagation()
     tem.isfinsh = !tem.isfinsh
     this.setList()
     // this.isfinsh
