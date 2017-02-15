b8a504e6964e46696bc359c10ed6b18c6d3c1c46
firstgit <505257618@qq.com>
2017-02-15 15:15:52 +0800

    关于简单的todolist以及任务列表（待完善）

--- src/components/footer/footer.controller.js
+++ src/components/footer/footer.controller.js
@@ -1,7 +1,15 @@
 export default class footerController {
-  constructor() {
+  constructor(localStorage, dateFilter) {
+    this.localStorage = localStorage
+    this.dateFilter = dateFilter
     this.name = 'footer'
-    this.item = [{bel: '123', isfinshed: true}, {bel: '321', isfinshed: false}]
+    this.item = []
+    this.isAllDone = false
+  }
+
+// 初始化
+  $onInit() {
+    this.getList()
   }
 
   change(x) {
@@ -14,10 +22,11 @@ export default class footerController {
       //  console.log(this.add)
       if (!this.noreapt(this.item, this.add)) {
         this.item.push({
-          bel: this.add, isfinshed: false
+          bel: this.add, isfinshed: false, id: this.item.length + 1
         })
       }
       this.add = ''
+      this.setList()
     }
   }
 
@@ -34,4 +43,57 @@ export default class footerController {
     }
     return false
   }
+
+  // 提取数据
+  getList() {
+    this.item = this.localStorage.fetch('Todo')
+  }
+
+  // 保存数据
+  setList() {
+    this.localStorage.save('Todo', this.item)
+  }
+
+  // 删除按钮
+  del(todo, e) {
+    e.stopPropagation()
+    this.item.splice(this.item.indexOf(todo), 1)
+    // this.newitem = this.item
+    // console.log(this.newitem)
+    // this.newSetLsit()
+    this.setList()
+  }
+
+  // 标记全部完成
+  alldone() {
+    angular.forEach(this.item, (x) => {
+      // console.log(this.isAllDone)
+      x.isfinshed = this.isAllDone
+    })
+    this.setList()
+  }
+
+  // 未完成的函数
+  uncomplete() {
+    var count = 0
+    angular.forEach(this.item, (x) => {
+      count += x.isfinshed ? 0 : 1
+    })
+    return count
+  }
+
+  // 已完成的函数
+  complete() {
+    var count = 0
+    angular.forEach(this.item, (x) => {
+      count += x.isfinshed ? 1 : 0
+    })
+    return count
+  }
+
+  // 时间搓
+  getNowTime() {
+    return dateFilter(new Date(), 'yyyy-MM-dd HH:mm:ss')
+  }
 }
+footerController.$inject = ['localStorage']
--- src/pages/home/home.controller.js
+++ src/pages/home/home.controller.js
@@ -1,16 +1,21 @@
 export default class HomeController {
-  constructor($http, $rootScope, $scope, userService, homeService, localStorage) {
+  constructor($http, $rootScope, $scope, $filter, userService, homeService, localStorage, dateFilter) {
     this.$http = $http
     this.$rootScope = $rootScope
     this.$scope = $scope
+    this.$filter = $filter
     this.user = userService
     this.home = homeService
     this.localStorage = localStorage
+    this.dateFilter = dateFilter
     this.item = ''
     this.items = []
     this.newItems = []
+    this.taskList = []
+    this.newTasklist = []
     // this.items = this.getdata()
   }
+
   // 初始化
   $onInit() {
     this.randomName()
@@ -18,11 +23,14 @@ export default class HomeController {
       console.log(resp.data)
     })
     this.getList()
+    // console.log(dateFilter((new Date(), 'yyyy-MM-dd HH:mm:ss')))
   }
+
   // 随机提取名字
   randomName() {
     this.name = this.user.getName()
   }
+
   // 删除按钮
   del(index, e) {
     e.stopPropagation()
@@ -36,6 +44,7 @@ export default class HomeController {
     this.t = this.localStorage.test()
     return this.t
   }
+
   stopBubble(e) {
     e.stopPropagation()
   }
@@ -44,14 +53,32 @@ export default class HomeController {
   getList() {
     this.items = this.localStorage.fetch('todo')
   }
+
+  // 新的TODO 提取数据
+  getTask() {
+    this.taskList = this.localStorage.fetch('todo')
+  }
+
   // 保存一个新的数据
   newSetLsit() {
     this.localStorage.save('todo', this.newItems)
   }
+
+  // TODO的保存新数据
+  newSetask() {
+    this.localStorage.save('todo', this.newTasklist)
+  }
+
   // 保存数据
   setList() {
     this.localStorage.save('todo', this.items)
   }
+
+  // 新的TODO 保存数据
+  savetask() {
+    this.localStorage.save('todo', this.taskList)
+  }
+
   // 输入回车后，保存数据
   enter(e) {
     // var keycode = window.event ? e.keyCode : e.which
@@ -65,16 +92,30 @@ export default class HomeController {
     }
   }
 
+  // TODO新的输入回车保存数据
+  // newEnter(e) {
+  //   if (e.keyCode === 13) {
+  //     console.log(this.newTasklist)
+  //     this.taskList.push({
+  //       id: this.taskList.length + 1, text: this.taskList, done: false, time: getNowTime()
+  //     })
+  //     this.newTasklist = ''
+  //     this.savetask()
+  //   }
+  // }
+
   clearItem() {
     console.log(this.items.length)
     console.log(this.localStorage.claerItem(this.items))
     console.log(this.items.length)
   }
+
   // 清空数据
   Clear() {
     this.localStorage.claer('todo')
     this.items = []
   }
+
   // 点击改变 样式
   change(tem, e) {
     e.stopPropagation()
@@ -82,5 +123,9 @@ export default class HomeController {
     this.setList()
   }
 
+  // 获取当前时间
+  getNowTime() {
+    return dateFilter(new Date(), 'yyyy-MM-dd HH:mm:ss')
+  }
 }
-HomeController.$inject = ['$http', '$rootScope', '$scope', 'user', 'home', 'localStorage']
+HomeController.$inject = ['$http', '$rootScope', '$scope', '$filter', 'user', 'home', 'localStorage', 'dateFilter']
